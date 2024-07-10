1fd8bcd87540ee5f1c01dbf4d0b167060799c75c
Diogo <diogodev00@gmail.com>
2024-07-10 17:57:00 -0300

    primeiro commit

--- projeto_1/settings.py
+++ projeto_1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h2cp!om4-dsxd@abt4%15@dlf*v#k3qvuax^7bro*l12h_*+4-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
